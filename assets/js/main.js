4b78fb7132b218ee8a6cae69ba1ef0df96c83283
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2018-12-19 15:27:03 +0100

    sketched a JS solution (not working until now)

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,12 @@ const isLeap = function(year) {
 
 let leap;
 for (let years = 2018; years < 2118; years++) {
+  // with mathematical functions
   leap = isLeap(years);
   console.log(`${years}: ${leap}`);
+  // with JS functions
+  // var leapy = new Date(years, 0, 29).getDay();
+  // if (leapy == 29) {
+  //   console.log(`${years} is a leap year`);
+  // }
 }
